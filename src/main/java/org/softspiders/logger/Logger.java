aaeaaaac57e754febfb71166eb2575a924c198a5
AlexanderLapygin <AlexanderLapygin@gmail.com>
2019-06-16 16:21:41 +0300

    feat: Just after logger creation its Writer list shouldn't be null test: Just after logger creation its Writer list shouldn't be null

--- src/main/java/org/softspiders/logger/Logger.java
+++ src/main/java/org/softspiders/logger/Logger.java
@@ -1,7 +1,18 @@
 package org.softspiders.logger;
 
+import java.io.Writer;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Logger implements LogService {
-    public Logger() {
-    }
+	List<Writer> writers = new ArrayList<Writer>();
+
+	public Logger() {
+	}
+
+	@Override
+	public List<Writer> getWriters() {
+		return writers;
+	}
 }
 
--- src/test/java/org/softspiders/logger/LoggerTest.java
+++ src/test/java/org/softspiders/logger/LoggerTest.java
@@ -1,10 +1,13 @@
 package org.softspiders.logger;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class LoggerTest {
+
+	// Creation tests
 
 	@Test
 	public void CalmCreation() {
@@ -14,5 +17,12 @@ public class LoggerTest {
 	@Test
 	public void LoggerIsInstanceOfLogService() {
 		assertTrue(new Logger() instanceof LogService);
+	}
+
+	// Writers basic tests
+
+	@Test
+	public void JustAfterLoggerCreationItsWriterListIsNotNull() {
+		Assert.assertNotNull((new Logger()).getWriters());
 	}
 }
