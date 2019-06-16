05c563cea0fae6caccfcdbf2822d0f05dbe85764
AlexanderLapygin <AlexanderLapygin@gmail.com>
2019-06-16 09:37:23 +0300

    feat: default creation test: default creation

---
+++ src/main/java/org/softspiders/logger/Logger.java
@@ -0,0 +1,7 @@
+package org.softspiders.logger;
+
+public class Logger {
+    public Logger() {
+    }
+}
+
--- src/test/java/org/softspiders/logger/LoggerTest.java
+++ src/test/java/org/softspiders/logger/LoggerTest.java
@@ -1,4 +1,12 @@
 package org.softspiders.logger;
 
+import org.junit.Test;
+
 public class LoggerTest {
+
+	@Test
+	public void CalmDefaultCreation() {
+		new Logger();
+	}
+
 }
